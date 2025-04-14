722a25fe4bc0a202626a2302e206965512b9b37d
mac323 <hvladyslav1234@gmail.com>
2025-04-14 16:41:04 +0300

    added all necessary firebase dependencies (analytics, authentication etc)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
